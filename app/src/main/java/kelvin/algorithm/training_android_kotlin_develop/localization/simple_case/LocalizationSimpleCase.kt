bafde86cd6c8e095e1ee794da62cb72d5cb5cadf
kelvin373-ht <kelvin373.ht@gmail.com>
2019-11-15 13:56:59 +0700

    UI Layout for Localization local-language

---
+++ app/src/main/java/kelvin/algorithm/training_android_kotlin_develop/localization/simple_case/LocalizationSimpleCase.kt
@@ -0,0 +1,13 @@
+package kelvin.algorithm.training_android_kotlin_develop.localization.simple_case
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kelvin.algorithm.training_android_kotlin_develop.R
+
+class LocalizationSimpleCase : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_localization_training)
+    }
+}
