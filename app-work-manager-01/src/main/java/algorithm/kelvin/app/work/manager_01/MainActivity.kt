66f1315de1f1d0ef4e47a82ae76190c558ba6e4b
kelvin373-ht <kelvin373.ht@gmail.com>
2020-02-17 13:52:38 +0700

    add module work manager 01

---
+++ app-work-manager-01/src/main/java/algorithm/kelvin/app/work/manager_01/MainActivity.kt
@@ -0,0 +1,12 @@
+package algorithm.kelvin.app.work.manager_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
