585f6dfa3e70d5683b50594e8c91847a6b31fc7b
kelvin373-ht <kelvin373.ht@gmail.com>
2020-02-19 10:45:23 +0700

    Make runnable in local activity

--- app-runnable-02/src/main/java/algorithm/kelvin/app/runnable_02/MainActivity.kt
+++ app-runnable-02/src/main/java/algorithm/kelvin/app/runnable_02/MainActivity.kt
@@ -2,11 +2,25 @@ package algorithm.kelvin.app.runnable_02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val handler = Handler()
+        val runnable = object: Runnable {
+            override fun run() {
+                Log.i("android-runnable", "Runnable is work")
+                handler.postDelayed(this, 250)
+            }
+        }
+        Log.i("android-runnable", "Runnable start")
+        handler.postDelayed(runnable, 5000)
+        Log.i("android-runnable", "You have been called Runnable")
+
     }
 }
