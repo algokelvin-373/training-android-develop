735849e38289dd2e9611273c4219dcdc3bc0ee8a
kelvin373-ht <kelvin373.ht@gmail.com>
2020-02-24 10:35:56 +0700

    Add method override fun AppCompatActiivity

--- app-activity-01/src/main/java/algorithm/kelvin/app/activity_01/MainActivity.kt
+++ app-activity-01/src/main/java/algorithm/kelvin/app/activity_01/MainActivity.kt
@@ -2,11 +2,42 @@ package algorithm.kelvin.app.activity_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("activity-one", "Start is run")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("activity-one", "Resume is run")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("activity-one", "Pause is run")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("activity-one", "Stop is run")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("activity-one", "Destroy is run")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("activity-one", "Restart is run")
     }
 }
