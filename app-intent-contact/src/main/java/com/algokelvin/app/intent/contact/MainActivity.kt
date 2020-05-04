7dadbe15e8a99f3c7c856b83c34a57e180647259
kelvin373-ht <kelvin373.ht@gmail.com>
2020-05-04 23:42:11 +0700

    Success get phone number

--- app-intent-contact/src/main/java/com/algokelvin/app/intent/contact/MainActivity.kt
+++ app-intent-contact/src/main/java/com/algokelvin/app/intent/contact/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.algokelvin.app.intent.contact
 
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -15,29 +16,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_contact.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK).apply {
-                type = ContactsContract.Contacts.CONTENT_TYPE
-            }
-            if (intent.resolveActivity(packageManager) != null) {
-                startActivityForResult(intent, REQUEST_SELECT_CONTACT)
-            }
+            val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+            intent.type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
-            val contactUri: Uri = data?.data ?: return
-            Log.i("uriContact", contactUri.toString())
-            val projection: Array<String> = arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER)
-            contentResolver.query(contactUri, null, null, null, null).use { cursor ->
-                //if the cursor return is valid, get the phone
-                if (cursor?.moveToFirst()!!) {
-                    val numberIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID)
-                    val name = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-                    val number = cursor.getString(numberIndex)
-                    Log.d("contact-number", "Detai : $number # ${name}")
-                }
+            val contactData: Uri = data?.data ?: return
+            val cursor: Cursor = managedQuery(contactData, null, null, null, null)
+            if (cursor.moveToFirst()) {
+                val id: String = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                val name: String = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val phone: String = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                Log.d("uriPhone", "$id $name $phone")
             }
         }
     }
