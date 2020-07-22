806bf446977d41c709d7120684957b5281abc403
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-22 11:06:04 +0700

    Project Training Android: Fragment TabLayout Optimization: Part Two ( in Fragment )

---
+++ App-Fragment-Tablayout-Optimization/app-two/src/main/java/com/algokelvin/training/android/fragment/tablayout/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.algokelvin.training.android.fragment.tablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
