745b26d1bd26c6f698cb07d53a2ccddaa5889cf2
kelvin373-ht <kelvin373.ht@gmail.com>
2020-08-26 16:47:23 +0700

    Add New Project Android Training: EditText Copy Paste Replace

---
+++ App-EditText-Copy-Paste-Replace/app/src/main/java/com/algokelvin/training/android/fragment/app_edittext_copy_paste_replace/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.algokelvin.training.android.fragment.app_edittext_copy_paste_replace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
