af2eb2398f1b38ce55dfdd762f3a2479e418dcc6
kelvin373-ht <kelvin373.ht@gmail.com>
2020-08-26 19:47:38 +0700

    Success Change EditText when text length is 5

--- App-EditText-Copy-Paste-Replace/app/src/main/java/com/algokelvin/training/android/fragment/app_edittext_copy_paste_replace/MainActivity.kt
+++ App-EditText-Copy-Paste-Replace/app/src/main/java/com/algokelvin/training/android/fragment/app_edittext_copy_paste_replace/MainActivity.kt
@@ -2,10 +2,32 @@ package com.algokelvin.training.android.fragment.app_edittext_copy_paste_replace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText_Number_Phone.transformationMethod = null
+        editText_Number_Phone.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length!! != 5) {
+                    editText_Number_Phone.removeTextChangedListener(this)
+                    editText_Number_Phone.setText("your text")
+                    editText_Number_Phone.addTextChangedListener(this)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
     }
 }
