27ebd5a4cc483c18727fad69ed0479e46a2be411
kelvin373-ht <kelvin373.ht@gmail.com>
2020-08-27 13:12:24 +0700

    Success Convert "+62" OR "62" to "0"

--- App-EditText-Copy-Paste-Replace/app/src/main/java/com/algokelvin/training/android/fragment/app_edittext_copy_paste_replace/MainActivity.kt
+++ App-EditText-Copy-Paste-Replace/app/src/main/java/com/algokelvin/training/android/fragment/app_edittext_copy_paste_replace/MainActivity.kt
@@ -7,6 +7,8 @@ import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var numberPhone = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,11 +16,12 @@ class MainActivity : AppCompatActivity() {
         editText_Number_Phone.transformationMethod = null
         editText_Number_Phone.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                if (s?.length!! != 5) {
-                    editText_Number_Phone.removeTextChangedListener(this)
-                    editText_Number_Phone.setText("your text")
-                    editText_Number_Phone.addTextChangedListener(this)
+                editText_Number_Phone.removeTextChangedListener(this)
+                when {
+                    numberPhone.startsWith("+62") -> editText_Number_Phone.setText(replaceRegexPhoneNumber("+62", "0", numberPhone))
+                    numberPhone.startsWith("62") -> editText_Number_Phone.setText(replaceRegexPhoneNumber62(numberPhone))
                 }
+                editText_Number_Phone.addTextChangedListener(this)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -26,8 +29,11 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
+                numberPhone = s.toString()
             }
         })
     }
+
+    fun replaceRegexPhoneNumber(regex: String, replaceRegex: String, numberPhone: String) = numberPhone.replace(regex, replaceRegex)
+    fun replaceRegexPhoneNumber62(numberPhone: String) = "0"+numberPhone.substring(2, numberPhone.length)
 }
